a071af7ee9da382a6eb963b0f17735f17aa2006c
VAIBHAV <68508494+vaibhavd2103@users.noreply.github.com>
2022-03-26 09:28:12 +0530

    [ADD] firestore users fetch on homescreen

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -2,32 +2,41 @@ import { StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import { db } from "../../firebase/firebase";
 
-const Home = () => {
-	const [info, setInfo] = useState([]);
-	const Fetchdata = () => {
-		db.collection("Users")
-			.get()
-			.then((querySnapshot) => {
-				// Loop through the data and store
-				// it in array to display
-				querySnapshot.forEach((element) => {
-					var data = element.data();
-					setInfo((arr) => [...arr, data]);
-				});
-			});
-	};
-	useEffect(() => {
-		Fetchdata();
-	}, []);
+const Home = (props) => {
+  const [users, setUsers] = useState([]);
+  console.log(props);
+  useEffect(() => {
+    const Fetchdata = async () => {
+      await db
+        .collection("Users")
+        .get()
+        .then((querySnapshot) => {
+          // console.log(querySnapshot.docs[0].id);
+          // querySnapshot.forEach((element) => {
+          //   var data = element.data();
+          //   setUsers((arr) => [...arr, data]);
+          // });
+          console.log(querySnapshot.docs[0].data());
+          setUsers(querySnapshot.docs);
+        });
+    };
+    Fetchdata();
+    db.collection("Users")
+      .doc(props.route.params.docID)
+      .get()
+      .then((currentUser) => {
+        console.log("====================", currentUser);
+      });
+  }, [db]);
 
-	return (
-		<View style={{ padding: 100 }}>
-			<Text>Home</Text>
-			{info.map((item) => {
-				return <Text>{item.name}</Text>;
-			})}
-		</View>
-	);
+  return (
+    <View style={{ padding: 100 }}>
+      <Text>Home</Text>
+      {users.map((item) => {
+        return <Text onPress={() => {}}>{item.data().name}</Text>;
+      })}
+    </View>
+  );
 };
 
 export default Home;
